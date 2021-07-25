a7e2ab5b8bfc8ced026feee439d9aaa851cc288e
yuan putra <bhalayuan@gmail.com>
2021-07-25 08:43:59 +0700

    route 0

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register API routes for your \application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
@@ -26,10 +26,10 @@ Route::prefix('/sanbox')->group(function(){
 
     Route::prefix('/app/{uuid}')->group(function(){
         Route::prefix('/message')->group(function(){
-            Route::get('/',[App\Http\Controllers\MessageCtrl,'index'])->name('.index');
-            Route::put('update/{id}',[App\Http\Controllers\MessageCtrl,'update'])->name('.update');
-            Route::delete('delete/{id}',[App\Http\Controllers\MessageCtrl,'destroy'])->name('.destroy');
-            Route::post('crete/',[App\Http\Controllers\MessageCtrl,'store'])->name('.store');
+            Route::get('/',[\App\Http\Controllers\MessageCtrl,'index'])->name('.index');
+            Route::put('update/{id}',[\App\Http\Controllers\MessageCtrl,'update'])->name('.update');
+            Route::delete('delete/{id}',[\App\Http\Controllers\MessageCtrl,'destroy'])->name('.destroy');
+            Route::post('crete/',[\App\Http\Controllers\MessageCtrl,'store'])->name('.store');
 
         })->name('.message');
 
